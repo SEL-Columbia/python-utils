9448efd2ea726fef2bb8695b9fccab7818e7cfdb
modilabs-dev <modilabs@googlegroups.com>
2012-06-21 15:15:02 -0400

    add init files

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from distutils.core import setup
 
 setup(
      name='modilabs-python-utils',
-     version='0.1.2',
+     version='0.1.3',
      author='modilabs',
      author_email='info@modilabs.org',
      packages=['modilabs', 'modilabs.utils'],
-     scripts=[],
      url='http://pypi.python.org/pypi/modilabs-python-utils/',
      license='LICENSE.txt',
      description='Collection of common Python utilities by Modi Group',
